5155871a9c553f9f2ca8a112e4030279df4bd10e
keyj <keyj@e92df349-602a-4b04-80d2-309aa873f826>
2009-04-29 13:00:00 +0000

    initial import of version 1.0

---
+++ trunk/nanojpeg/win32/clean.bat
@@ -0,0 +1,13 @@
+attrib -h *.*
+del *.ncb
+del *.pdb
+del *.suo
+del *.ilk
+del *.user
+del *_crinkler.html
+del *_Debug.exe
+del /q Debug\*.*
+del /q Release\*.*
+rd Debug
+rd Release
+del nanojpeg_out.*
